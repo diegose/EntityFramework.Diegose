fb26e80069d63856813dae5e407b13adb7ab0eee
Diego Mijelshon <diego@mijelshon.com.ar>
2012-09-20 17:20:56 -0300

    Failing test for #1

--- EntityFramework.Diegose.Test/Issue1/Fixture.cs
+++ EntityFramework.Diegose.Test/Issue1/Fixture.cs
@@ -1,41 +1,65 @@
-﻿using System.Data.Entity.Validation;
+﻿using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using NUnit.Framework;
 
 namespace EntityFramework.Diegose.Test.Issue1
 {
-    [TestFixture]
-    public class WhenARequiredReferenceIsNotLoaded
+    public abstract class WhenARequiredReferenceIsNotLoaded<TContext> where TContext : DbContext, new()
     {
         [TestFixtureSetUp]
         public void CreateData()
         {
-            using (var context = new StandardContext())
+            using (var context = CreateContext())
             {
                 context.Database.Delete();
                 context.Database.Create();
-                context.Foos.Add(new Foo { Bar = new Bar() });
+                context.Set<Foo>().Add(new Foo { Bar = new Bar() });
                 context.SaveChanges();
             }
+        }
+
+        protected TContext CreateContext()
+        {
+            return new TContext();
         }
 
         [TestFixtureTearDown]
         public void Cleanup()
         {
-            using (var context = new StandardContext())
+            using (var context = CreateContext())
             {
                 context.Database.Delete();
             }
         }
+    }
 
+    [TestFixture]
+    public class WhenARequiredReferenceIsNotLoadedWithStandardContext : WhenARequiredReferenceIsNotLoaded<StandardContext>
+    {
         [Test]
         public void DbContextSaveChangesFails()
         {
-            using (var context = new StandardContext())
+            using (var context = CreateContext())
             {
-                var foo = context.Foos.Single();
+                var foo = context.Set<Foo>().Single();
                 foo.FooData = 1;
                 Assert.That(() => context.SaveChanges(), Throws.InstanceOf<DbEntityValidationException>());
+            }
+        }
+    }
+
+    [TestFixture]
+    public class WhenARequiredReferenceIsNotLoadedWithMyContext : WhenARequiredReferenceIsNotLoaded<MyContext>
+    {
+        [Test]
+        public void DbContextSaveChangesSucceeds()
+        {
+            using (var context = CreateContext())
+            {
+                var foo = context.Set<Foo>().Single();
+                foo.FooData = 1;
+                context.SaveChanges();
             }
         }
     }
---
+++ EntityFramework.Diegose.Test/Issue1/MyContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace EntityFramework.Diegose.Test.Issue1
+{
+    public class MyContext : DiegoseContext
+    {
+        public IDbSet<Foo> Foos { get; set; }
+    }
+}
