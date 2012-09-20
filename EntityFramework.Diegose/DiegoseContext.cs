f09323e270165202fe45be814a2469fa5d613395
Diego Mijelshon <diego@mijelshon.com.ar>
2012-09-20 17:24:54 -0300

    Closed #1

--- EntityFramework.Diegose/DiegoseContext.cs
+++ EntityFramework.Diegose/DiegoseContext.cs
@@ -1,9 +1,28 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
 
 namespace EntityFramework.Diegose
 {
     public class DiegoseContext : DbContext
     {
-         
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var property in from result in ex.EntityValidationErrors
+                                         from error in result.ValidationErrors
+                                         select new { error.PropertyName, result.Entry })
+                {
+                    try { property.Entry.Reference(property.PropertyName).Load(); }
+                    catch { }
+                }
+                return base.SaveChanges();
+            }
+        }
     }
 }
