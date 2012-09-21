d2f7b9de5e6937194717b328746607664023d289
Diego Mijelshon <diego@mijelshon.com.ar>
2012-09-21 13:33:00 -0300

    Closed #2

--- EntityFramework.Diegose.Test/Issue2/MyContext.cs
+++ EntityFramework.Diegose.Test/Issue2/MyContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Linq;
 
 namespace EntityFramework.Diegose.Test.Issue2
 {
@@ -6,9 +7,18 @@ namespace EntityFramework.Diegose.Test.Issue2
     {
         public IDbSet<Foo> Foos { get; set; }
 
-        public int ExecuteNonQuery(string sql, object paramters)
+        public int ExecuteNonQuery(string sql, object parameters)
         {
-            throw new System.NotImplementedException();
+            var parameterFactory = Database.Connection.CreateCommand();
+            return Database.ExecuteSqlCommand(sql, parameters.GetType().GetProperties()
+                             .Select(x =>
+                                         {
+                                             var parameter = parameterFactory.CreateParameter();
+                                             parameter.ParameterName = x.Name;
+                                             parameter.Value = x.GetValue(parameters, null);
+                                             return (object) parameter;
+                                         })
+                             .ToArray());
         }
     }
 }
